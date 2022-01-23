c03777e56b415dfd8f5c33c87994762321f4f78e
Gowtham <gokulgowtham001@gmail.com>
2022-01-23 22:24:17 +0530

    updated

--- mod4_soln/SpeakHello.js
+++ mod4_soln/SpeakHello.js
@@ -17,7 +17,7 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 helloSpeaker.speak=function(name){
   console.log(speakWord + " " + name);
-}
+};
 window.helloSpeaker=helloSpeaker;
 
 })(window);
